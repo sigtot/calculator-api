cab203d1c771351e1dead1948bfe03711f2d2154
sigtot <sigurdtotland@gmail.com>
2018-10-10 23:32:36 +0200

    Implement eval function

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -11,6 +15,37 @@ const apiRoot = "/api/"
 
 type CalcRequest struct {
 	Expression string `json:"expression"`
+}
+
+func eval(node ast.Node) int {
+	switch n := node.(type) {
+	case *ast.BinaryExpr:
+		switch n.Op {
+		case token.ADD:
+			return eval(n.X) + eval(n.Y)
+		case token.SUB:
+			return eval(n.X) - eval(n.Y)
+		case token.MUL:
+			return eval(n.X) * eval(n.Y)
+		case token.QUO:
+			return eval(n.X) / eval(n.Y)
+		}
+	case *ast.UnaryExpr:
+		if n.Op == token.SUB {
+			return -eval(n.X)
+		}
+	case *ast.BasicLit:
+		if n.Kind == token.INT || n.Kind == token.FLOAT {
+			val, err := strconv.Atoi(n.Value)
+			if err != nil {
+				fmt.Println(err) // TODO: We should bubble this error up
+			}
+			return val
+		}
+	case *ast.ParenExpr:
+		return eval(n.X)
+	}
+	return 0 // TODO: Unhandled type: should return error
 }
 
 func handleCalc(w http.ResponseWriter, r *http.Request) {
@@ -23,7 +58,16 @@ func handleCalc(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "500 internal server error", http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(request.Expression)
+
+	fileSet := token.NewFileSet()
+	tree, err := parser.ParseExpr(request.Expression)
+	if err != nil {
+		fmt.Println("Handled error:", err) // TODO: Remove after debugging
+		http.Error(w, "400 bad request", http.StatusBadRequest)
+		return
+	}
+	ast.Print(fileSet, tree)
+	fmt.Println("Result:", eval(tree))
 }
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
