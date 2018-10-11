80a635dc13e803a44c21a5cdbfa433706d47a49d
sigtot <sigurdtotland@gmail.com>
2018-10-11 22:58:02 +0200

    Add api test

---
+++ api_test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+go build
+
+echo "Starting API..."
+./rest-calculator & # Start the server process
+export PID=$! # Save the process id for termination
+
+echo "Sending expression '-1 * (2 * 6 / 3)'..."
+echo "Expecting result -4"
+result="$(curl -s -X POST -H 'Content-Type: application/json' -d '{"expression": "-1 * (2 * 6 / 3)"}' http://127.0.0.1:8000/api/calc/)" # Test the api
+
+echo "Received response ${result}"
+
+echo "Killing server and deleting binary..."
+kill -15 ${PID} # Kill the server process
+rm rest-calculator # Delete binary
+
+echo "Test complete"
